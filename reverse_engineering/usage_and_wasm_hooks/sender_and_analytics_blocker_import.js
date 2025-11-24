31b948a5a4f4d23abb4491b6d7a84bfef65568c8
Lucrehulk <97923189+Lucrehulk@users.noreply.github.com>
2025-11-24 18:22:20 -0500

    Update sender_and_analytics_blocker_import.js

--- reverse_engineering/usage_and_wasm_hooks/sender_and_analytics_blocker_import.js
+++ reverse_engineering/usage_and_wasm_hooks/sender_and_analytics_blocker_import.js
@@ -1,4 +1,5 @@
 (index, slice) => {
+    // T packet header "84" detector and blocker
     if (u8[index] == 84) {
         window.analytics_data = JSON.parse(new TextDecoder().decode(u8.slice(index + 4, index + slice)));
         window.user_id = analytics_data.storage.id;
@@ -11,6 +12,7 @@
         new_message = new TextEncoder().encode(new_message);
         u8.set(new_message, index + 4);
     };
+    // This sends any packets from our queue by replacing the packet in the memory with our modified packet, and so as to not cause errors we need to return the length of the packet (which is also used by the webassembly)
     let new_packet = window.packet_queue.pop();
     if (new_packet) {
         u8.set(new_packet, 0);
