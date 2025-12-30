778207c33f048b80454a0a2313ba8edb82642493
Lucrehulk <97923189+Lucrehulk@users.noreply.github.com>
2025-12-30 17:39:25 -0500

    Update control_packet_constructor.js

--- reverse_engineering/packet_sending_constructors/control_packet_constructor.js
+++ reverse_engineering/packet_sending_constructors/control_packet_constructor.js
@@ -1,7 +1,8 @@
 // Very incomplete. Mainly just looked into utilizing this for facing mechanics. I did facing position does not extend outside 1 byte range (if it is far away from center it may extend to two bytes for x and y facing in the packet). 
 // Can use in the form of x = cos(theta), y = sin(theta) polar type coords, but encoded in a manner so they range from 0-191
-// Xleft of center (0) = values decreasing starting from 191, and for Xright of center it increases from 0.
-// Same concept for Ys, upwards = decreasing from 191
+// For one byte values, which is all I wanted since I wanted angles, X will go from 191-128 decreasing as you go left of center, and for right of center it goes 1-127 both are inclusive, 0 is center obv.
+// Same concept applies to Ys but it goes 191-128 for upwards, and 1-127 downwards.
+// Sine ranges are uneven we can take the smaller range (191-128) and construct a facing angle for the 64 value range (so 191-128, 0-64).
 // Direction is a code which tells the tank how it will move, literally just for movement but I didn't map them all out here, but like for example 1 = only up pretty sure.
 function construct_control_packet(x_comp, y_comp, direction) {
   return new Uint8Array([67, x_comp, y_comp, direction]);
