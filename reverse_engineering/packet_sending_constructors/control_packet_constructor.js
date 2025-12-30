3cc6ce937698845f45293eabce06a7159fb1aa9c
Lucrehulk <97923189+Lucrehulk@users.noreply.github.com>
2025-12-30 17:46:28 -0500

    Update control_packet_constructor.js

--- reverse_engineering/packet_sending_constructors/control_packet_constructor.js
+++ reverse_engineering/packet_sending_constructors/control_packet_constructor.js
@@ -7,3 +7,14 @@
 function construct_control_packet(x_comp, y_comp, direction) {
   return new Uint8Array([67, x_comp, y_comp, direction]);
 }
+
+// Adding this. Method for calculating the angle based off of what was described earlier.
+function yield_control_comps_from_angle(angle) {
+    let cartesian_x_comp = Math.cos(angle);
+    let cartesian_y_comp = Math.sin(angle);
+    let x_comp = Math.floor(cartesian_x_comp * 64);
+    let y_comp = Math.floor(cartesian_y_comp * 64);
+    if (cartesian_x_comp < 0) x_comp = 191 - x_comp;
+    if (cartesian_y_comp > 0) y_comp = 191 - y_comp;
+    return [x_comp, y_comp];
+}
